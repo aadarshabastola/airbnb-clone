d197df80d7dfe61abc5da4ef8002751e28be3d7f
Aadarsha Bastola <aadarsha.com@gmail.com>
2024-01-07 16:15:30 -0700

    Navigation Bar, Auth, DB, Categories UI

---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import useRentModal from "@/app/hooks/useRentModal";
+import Modal from "./modal";
+import { useState } from "react";
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5,
+}
+
+const RentModal = () => {
+    const rentModal = useRentModal();
+
+    const [ step, setStep ] = useState(STEPS.CATEGORY);
+
+    const onBack = () => {
+        setStep((value) => value - 1);
+    }
+
+    const onNext = () => {
+        setStep((value) => value + 1);
+    }
+    
+    return ( 
+        <Modal 
+            title="Airbnb your home!"
+            isOpen={rentModal.isOpen}
+            onClose={rentModal.onClose}
+            onSubmit={rentModal.onClose}
+            actionLabel="Submit"
+        />
+     );
+}
+ 
+export default RentModal;
